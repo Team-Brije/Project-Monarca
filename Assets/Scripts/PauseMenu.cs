dfef3e483ce85ff5e4cf688428865315b1aec1ce
Bl4ke36 <3du4rdo.pv@gmail.com>
2024-11-10 17:27:00 -0600

    Menús
    Cuando le das Play desde el menú de inicio se bugean la iluminación

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class Settings : MonoBehaviour
+public class PauseMenu : MonoBehaviour
 {
+    public GameObject pauseMenu;
     public GameObject settingsMenu;
-    public GameObject menu;
+    public bool menuON;
+
+    public AudioMixer music;
+    public AudioMixer ambient;
     Resolution[] resolutions;
     public TMP_Dropdown resDropdown;
+
     void Start()
     {
         resolutions = Screen.resolutions;
@@ -30,12 +35,26 @@ public class Settings : MonoBehaviour
                 currentRes = i;
             }
         }
-
         resDropdown.AddOptions(options);
         resDropdown.value = currentRes;
         resDropdown.RefreshShownValue();
     }
 
+    private void Update()
+    {
+
+        if (Input.GetKeyUp(KeyCode.Escape) && !menuON)
+        {
+            pauseMenu.SetActive(true);
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            menuON = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.Escape) && menuON)
+        {
+            Resume();
+        }
+    }
     public void SetRes(int resIndex)
     {
         Resolution resolution = resolutions[resIndex];
@@ -46,23 +65,36 @@ public class Settings : MonoBehaviour
         Screen.fullScreen = isFS;
     }
 
-    public void SettingsMenu()
+    public void SetMusicVolume(float volume)
+    {
+        //Debug.Log(volume);
+        music.SetFloat("Volume",volume);
+    }
+    public void SetAmbientVolume(float volume)
+    {
+        //Debug.Log(volume);
+        ambient.SetFloat("Volume", volume);
+    }
+
+    public void OpenSettings()
     {
         settingsMenu.SetActive(true);
-        menu.SetActive(false);
-
     }
-    public void ExitSettingsMenu()
+    public void CloseSettings()
     {
-        menu.SetActive(true);
         settingsMenu.SetActive(false);
     }
-    public void Play()
+    public void Resume()
     {
-        SceneManager.LoadScene(1);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        pauseMenu.SetActive(false);
+        settingsMenu.SetActive(false);
+        menuON = false;
     }
-    public void Quit()
+
+    public void MainMenu()
     {
-        Application.Quit();
+        SceneManager.LoadScene(0);
     }
 }
